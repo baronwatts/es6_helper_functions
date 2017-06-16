93f4382c020afd78745780badf37bdd5b3f60b7e
Baron Watts <watts.baron@hotmail.com>
2017-06-16 14:35:06 -0700

    Update es6_helper_functions.js

--- es6_helper_functions.js
+++ es6_helper_functions.js
@@ -71,6 +71,8 @@ countWords("hello your reading about composition"); //=> 5
 //Create range
 let range = (start, end) => Array.from({ length: end + 1 - start }, (v, k) => start + k);
 range(5, 10); //=> [5, 6, 7, 8, 9, 10]
+range(5, 10).reverse(); //=> [10, 9, 8, 7, 6, 5]
+
 
 
 //Generate a Random Number between..
