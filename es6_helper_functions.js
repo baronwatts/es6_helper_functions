69ed88f4a143e5b9050cdde50e5a51328e6ea153
Baron Watts <watts.baron@hotmail.com>
2017-07-04 08:53:43 -0700

    Update es6_helper_functions.js

--- es6_helper_functions.js
+++ es6_helper_functions.js
@@ -239,3 +239,10 @@ var obj = {
 for ( const [key, value] of Object.entries(obj) ) {
     console.log(`${key}: ${value}`); //=> outputs the object as a string
 }
+
+
+//Create an array of objects
+let arrOfObj = new Array(5).fill(null).map(i => 
+	({ 'age': randnum(18, 65), 'city': Math.random() >= 0.50 ? 'San Diego' : 'Texas' }));
+
+
