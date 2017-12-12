cf70f16f49505dcbdfe466b849cb1137d203c5e3
Baron Watts <watts.baron@hotmail.com>
2017-12-12 10:05:13 -0800

    Update es6_helper_functions.js

--- es6_helper_functions.js
+++ es6_helper_functions.js
@@ -178,6 +178,14 @@ let findColor = name => ({ red:'#ff4444', blue: '#3b5998', yellow: '#fff68f' })[
 findColor('red'); //=> #ff4444
 
 
+//UUID Generator
+let uuid = _ =>
+  ( [1e7]+-1e3+-4e3+-8e3+-1e11 ).replace( /[018]/g, c =>
+    (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+  );
+// uuid() -> '7982fcfe-5721-4632-bede-6000885be57d'
+
+
 
 //Object to Array & Array to Object
 let our_data = {
